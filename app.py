6e50ee65273bc939b42c63f8c7bd5aa651a36c81
mohsinwani42-dot <mohsinwani42@gmail.com>
2026-06-20 15:25:01 +0530

    full
    null

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 from datetime import datetime
 import json
 import os
 
 app = Flask(__name__, static_folder='.', static_url_path='')
+CORS(app)
 DATA_FILE = 'submissions.json'
 
 @app.route('/')
